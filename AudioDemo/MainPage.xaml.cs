c23da1d9765417cd103b28da7eace2998c782f9f
madeinouweland <loek2015@outlook.com>
2015-06-09 12:25:45 +0200

    initial

---
+++ AudioDemo/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace AudioDemo {
+    public sealed partial class MainPage : Page {
+        public MainPage() {
+            this.InitializeComponent();
+        }
+
+        private AudioPlayer _player = new AudioPlayer();
+
+        protected async override void OnNavigatedTo(NavigationEventArgs e) {
+            base.OnNavigatedTo(e);
+            var wav = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/Audio/washburn.wav"));
+            var wma = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/Audio/closure.wma"));
+            var mp3 = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/Audio/oldbones.mp3"));
+
+            await _player.LoadFileAsync(wav);
+            await _player.LoadFileAsync(wma);
+            await _player.LoadFileAsync(mp3);
+
+        }
+
+        private void ButtonWAV_Click(object sender, RoutedEventArgs e) {
+            _player.Play("washburn.wav", SliderWAV.Value / 100);
+        }
+
+        private void ButtonWMA_Click(object sender, RoutedEventArgs e) {
+            _player.Play("closure.wma", SliderWMA.Value / 100);
+        }
+
+        private void ButtonMP3_Click(object sender, RoutedEventArgs e) {
+            _player.Play("oldbones.mp3", SliderMP3.Value / 100);
+        }
+    }
+}
